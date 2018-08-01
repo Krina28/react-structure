1e943d9df98d2171ea26bc1e908cb50b5503f0ca
Herat Patel <herat.patel@bacancytechnology.com>
2018-08-01 10:39:00 +0530

    Initial commit

---
+++ src/Common/footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './common.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="main-footer" />
+	        <div className="footer-content">
+	            Footer
+	        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Common/header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, Button, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+		  <Navbar.Header>
+		    <Link to="/">React-App</Link>
+		    <Image src="../src/logo.svg" circle />
+		  </Navbar.Header>
+	      <Nav>
+	        <NavItem>
+	          <Button><Link to="/login">Login</Link></Button>
+	          <Button><Link to="/sign-up">Signup</Link></Button>
+	        </NavItem >
+	      </Nav>
+		</Navbar>
+		<hr />
+      </div>
+    );
+  }
+}
+
+export default Header;
